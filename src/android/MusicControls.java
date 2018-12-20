b6117a87e7cff67893bed381f0ca0da0bf7404fc
Shoaib Khan <skkshoaib@gmail.com>
2018-12-21 07:41:42 +0800

    refs #master Added support for bluetooth headset plugged/unplugged

--- src/android/MusicControls.java
+++ src/android/MusicControls.java
@@ -4,12 +4,11 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
-import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.util.Log;
+import android.bluetooth.BluetoothDevice;
 import android.app.Activity;
 import android.content.Context;
 import android.content.IntentFilter;
@@ -17,12 +16,7 @@ import android.content.Intent;
 import android.app.PendingIntent;
 import android.content.ServiceConnection;
 import android.content.ComponentName;
-import android.app.Service;
 import android.os.IBinder;
-import android.os.Bundle;
-import android.os.Build;
-import android.R;
-import android.content.BroadcastReceiver;
 import android.media.AudioManager;
 
 public class MusicControls extends CordovaPlugin {
@@ -36,26 +30,28 @@ public class MusicControls extends CordovaPlugin {
 
 	private void registerBroadcaster(MusicControlsBroadcastReceiver mMessageReceiver){
 		final Context context = this.cordova.getActivity().getApplicationContext();
-		context.registerReceiver((BroadcastReceiver)mMessageReceiver, new IntentFilter("music-controls-previous"));
-		context.registerReceiver((BroadcastReceiver)mMessageReceiver, new IntentFilter("music-controls-pause"));
-		context.registerReceiver((BroadcastReceiver)mMessageReceiver, new IntentFilter("music-controls-play"));
-		context.registerReceiver((BroadcastReceiver)mMessageReceiver, new IntentFilter("music-controls-next"));
-		context.registerReceiver((BroadcastReceiver)mMessageReceiver, new IntentFilter("music-controls-media-button"));
-		context.registerReceiver((BroadcastReceiver)mMessageReceiver, new IntentFilter("music-controls-destroy"));
+		context.registerReceiver(mMessageReceiver, new IntentFilter("music-controls-previous"));
+		context.registerReceiver(mMessageReceiver, new IntentFilter("music-controls-pause"));
+		context.registerReceiver(mMessageReceiver, new IntentFilter("music-controls-play"));
+		context.registerReceiver(mMessageReceiver, new IntentFilter("music-controls-next"));
+		context.registerReceiver(mMessageReceiver, new IntentFilter("music-controls-media-button"));
+		context.registerReceiver(mMessageReceiver, new IntentFilter("music-controls-destroy"));
 
 		// Listen for headset plug/unplug
-		context.registerReceiver((BroadcastReceiver)mMessageReceiver, new IntentFilter(Intent.ACTION_HEADSET_PLUG));
+		context.registerReceiver(mMessageReceiver, new IntentFilter(Intent.ACTION_HEADSET_PLUG));
+        context.registerReceiver(mMessageReceiver, new IntentFilter(BluetoothDevice.ACTION_ACL_CONNECTED));
+        context.registerReceiver(mMessageReceiver, new IntentFilter(BluetoothDevice.ACTION_ACL_DISCONNECTED));
 	}
 
 	// Register pendingIntent for broacast
 	public void registerMediaButtonEvent(){
-		if (this.mediaButtonAccess && android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR2){
+		if (this.mediaButtonAccess){
 			this.mAudioManager.registerMediaButtonEventReceiver(this.mediaButtonPendingIntent);
 		}
 	}
 
 	public void unregisterMediaButtonEvent(){
-		if (this.mediaButtonAccess && android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR2){
+		if (this.mediaButtonAccess){
 			this.mAudioManager.unregisterMediaButtonEventReceiver(this.mediaButtonPendingIntent);
 		}
 	}
